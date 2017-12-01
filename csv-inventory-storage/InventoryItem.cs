235f91152d6b55279abb4bcbab9f589fd4e70c4a
ffjeanlucwiegmann <jean-luc.wiegmann@flowfact.de>
2017-12-01 09:59:38 +0100

    refactoring of model

--- csv-inventory-storage/InventoryItem.cs
+++ csv-inventory-storage/InventoryItem.cs
@@ -2,53 +2,50 @@
 
 namespace CSVInventoryStorage
 {
-    class InventoryItem
+    internal class InventoryItem
     {
-        private string _Description;
-        private string _InventoryGroup;
-        private string _InventoryId;
-        private string _SerialNumber;
-        private DateTime _AddedAt;
-        private string _AddedBy;
+        private string _description;
+        private string _inventoryGroup;
+        private string _inventoryId;
+        private string _serialNumber;
+        private string _addedBy;
 
         public string Description {
-            get { return _Description; }
-            set { _Description = Sanitize(value); }
+            get => _description;
+            set => _description = Sanitize(value);
         }
         public string InventoryGroup
         {
-            get { return _InventoryGroup; }
-            set { _InventoryGroup = Sanitize(value); }
+            get => _inventoryGroup;
+            set => _inventoryGroup = Sanitize(value);
         }
         public string InventoryId
         {
-            get { return _InventoryId; }
-            set { _InventoryId = Sanitize(value); }
+            get => _inventoryId;
+            set => _inventoryId = Sanitize(value);
         }
         public string SerialNumber
         {
-            get { return _SerialNumber; }
-            set { _SerialNumber = Sanitize(value); }
+            get => _serialNumber;
+            set => _serialNumber = Sanitize(value);
         }
-        public DateTime AddedAt
-        {
-            get { return _AddedAt;  }
-            set { _AddedAt = value; }
-        }
+        public DateTime AddedAt { get; set; }
+
         public string AddedBy
         {
-            get { return _AddedBy; }
-            set { _AddedBy = Sanitize(value); }
+            get => _addedBy;
+            set => _addedBy = Sanitize(value);
         }
 
-        public string ToCSV()
+        public string ToCsv()
         {
-            return string.Format("{0};{1};{2};{3};{4};{5}", _Description, _InventoryGroup, _InventoryId, _SerialNumber, _AddedAt.ToString("dd.MM.yyyy"), _AddedBy);
+            return
+                $"{_description};{_inventoryGroup};{_inventoryId};{_serialNumber};{AddedAt:dd.MM.yyyy};{_addedBy}";
         }
 
-        public static InventoryItem FromCSV(string csv)
+        public static InventoryItem FromCsv(string csv)
         {
-            string[] parsed = csv.Split(';');
+            var parsed = csv.Split(';');
 
             return new InventoryItem()
             {
