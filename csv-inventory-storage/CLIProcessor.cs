5e0df40a46843d41ff65abf57eb62c16d43d493b
ffjeanlucwiegmann <jean-luc.wiegmann@flowfact.de>
2017-12-01 10:18:45 +0100

    more refactoring

--- csv-inventory-storage/CLIProcessor.cs
+++ csv-inventory-storage/CLIProcessor.cs
@@ -7,13 +7,25 @@ namespace CSVInventoryStorage
     /// <summary>
     /// Commandline processor.
     /// </summary>
-    public class CLIProcessor
+    public class CliProcessor
     {
-        private static List<ICommand> m_lCommands = new List<ICommand>();
+        private static readonly List<ICommand> MlCommands = new List<ICommand>();
 
-        private static Dictionary<string, Func<object[], string>> m_dLambdaCommands = new Dictionary<string, Func<object[], string>>() {
-            { "help", (object[] args) => { return "Usage: <command|operator|function> [<operands...>]\nExample input: + 5 10 15\n\nAvailable commands:\n  help - Displays available commands\n  exit - Exits SimpleCalc\n\nAvailable operators:\n  + - Adds two or more operands\n  - - Subtracts two or more operands\n  * - Multiplies two or more numbers\n  / - Divides two or more numbers\n\nAvailable functions:\n  sin - Calculates the sinus of a number\n  cos - Calculates the cosinus of a number\n  tan - Calculates the tangent of a number\n  deg2rad - Converts an angle from degrees to radians\n\nAvailable placeholders:\n  &l - Reference to the last result"; } },
-            { "exit", (object[] args) => { Environment.Exit(0); return "Exiting..."; } }
+        private static readonly Dictionary<string, Func<object[], string>> MdLambdaCommands = 
+            new Dictionary<string, Func<object[], string>>() {
+                { "help", (object[] args) 
+                    => "Usage: <command|operator|function> [<operands...>]\n" +
+                       "Example input: + 5 10 15\n\nAvailable commands:\n  " +
+                       "help - Displays available commands\n  exit - Exits SimpleCalc\n\n" +
+                       "Available operators:\n  + - Adds two or more operands\n  - - " +
+                       "Subtracts two or more operands\n  * - Multiplies two or more numbers\n" +
+                       "  / - Divides two or more numbers\n\nAvailable functions:\n  sin - " +
+                       "Calculates the sinus of a number\n  cos - Calculates the cosinus of a number\n  " +
+                       "tan - Calculates the tangent of a number\n  deg2rad - " +
+                       "Converts an angle from degrees to radians\n\nAvailable placeholders:\n  " +
+                       "&l - Reference to the last result"},
+                { "exit", (object[] args) 
+                    => { Environment.Exit(0); return "Exiting..."; } }
         };
 
         /// <summary>
@@ -22,10 +34,10 @@ namespace CSVInventoryStorage
         /// <param name="command">Instance of the command to register.</param>
         public static void RegisterCommand(ICommand command)
         {
-            if (m_lCommands.Contains(command))
+            if (MlCommands.Contains(command))
                 throw new CommandRegisterException("Command already registered");
 
-            m_lCommands.Add(command);
+            MlCommands.Add(command);
         }
 
         /// <summary>
@@ -35,10 +47,10 @@ namespace CSVInventoryStorage
         /// <param name="handler">Command handler delegate to register.</param>
         public static void RegisterCommand(string commandName, Func<object[], string> handler)
         {
-            if (m_dLambdaCommands.ContainsKey(commandName))
+            if (MdLambdaCommands.ContainsKey(commandName))
                 throw new CommandRegisterException("Command already registered");
 
-            m_dLambdaCommands.Add(commandName, handler);
+            MdLambdaCommands.Add(commandName, handler);
         }
 
         /// <summary>
@@ -47,10 +59,10 @@ namespace CSVInventoryStorage
         /// <param name="command">Command to unregister.</param>
         public static void UnregisterCommand(ICommand command)
         {
-            if (!m_lCommands.Contains(command))
+            if (!MlCommands.Contains(command))
                 throw new CommandRegisterException("Command not registered");
 
-            m_lCommands.Remove(command);
+            MlCommands.Remove(command);
         }
 
         /// <summary>
@@ -59,10 +71,10 @@ namespace CSVInventoryStorage
         /// <param name="commandName">Name of the command to unregister.</param>
         public static void UnregisterCommand(string commandName)
         {
-            if (!m_dLambdaCommands.ContainsKey(commandName))
+            if (!MdLambdaCommands.ContainsKey(commandName))
                 throw new CommandRegisterException("Command not registered");
 
-            m_dLambdaCommands.Remove(commandName);
+            MdLambdaCommands.Remove(commandName);
         }
 
         /// <summary>
@@ -72,24 +84,25 @@ namespace CSVInventoryStorage
         /// <returns>The command handler or operation response.</returns>
         public static string Process(string input)
         {
-            string[] split = input.Split(' ');
+            var split = input.Split(' ');
             if (split.Length == 0)
                 throw new InvalidArgumentException("Invalid command");
 
-            string command = split[0];
+            var command = split[0];
 
-            object[] args = (object[])split.Skip(1).ToArray();
+            var args = (object[])split.Skip(1).ToArray();
 
-            ICommand cmd = m_lCommands.Find(x => x.CommandName() == command);
+            ICommand cmd = MlCommands.Find(x => x.CommandName() == command);
             if (cmd != null)
             {
                 if (cmd.ArgCount() != -1 && args.Length != cmd.ArgCount())
                     return cmd.Usage();
+
                 return cmd.Action(args);
             }
 
-            if (m_dLambdaCommands.ContainsKey(command))
-                return m_dLambdaCommands[command](args);
+            if (MdLambdaCommands.ContainsKey(command))
+                return MdLambdaCommands[command](args);
 
             throw new ProcessingException("Unknown operation or invalid arguments");
         }
